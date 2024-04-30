96966cf54f2ee784954af60d9478c33e0edfc44a
shike <imshike@gmail.com>
2024-04-30 22:34:57 +0800

    optimize build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "=== begin build files $* ..."
 
-g++ main.cpp src/imgui.cpp src/* --std=c++17 -I ./include -I ./include/imGui  -I ./include/imGui/backends -I../../code/glfw/include -L ../../code/glfw/build/src -lglfw3 -framework OpenGL -framework Cocoa -framework CoreFoundation -framework IOKit
-
+#g++ main.cpp src/imgui.cpp src/* --std=c++17 -I ./include -I ./include/imGui  -I ./include/imGui/backends -I../../code/glfw/include -L ../../code/glfw/build/src -lglfw3 -framework OpenGL -framework Cocoa -framework CoreFoundation -framework IOKit
+g++ -c main.cpp src/*  --std=c++17 -I ./include -I ./include/imGui  -I ./include/imGui/backends -I../../code/glfw/include      
+g++ *.o -L ../../code/glfw/build/src -lglfw3 -framework OpenGL -framework Cocoa -framework CoreFoundation -framework IOKit
+rm *.o
 echo "=== build files finished ..."
 
